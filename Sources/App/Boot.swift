8f9bd21570fcdc5eb48484d445beaac81aef9cdc
shenfu1991 <xuanyuanshenfu@outlook.com>
2023-07-07 09:58:04 +0800

    ad

--- Sources/App/Boot.swift
+++ Sources/App/Boot.swift
@@ -18,9 +18,11 @@ var sbIdx = 0
 let sbArr = ["BTCUSDT","ETHUSDT","TOMOUSDT","ALPHAUSDT","NKNUSDT","RSRUSDT","GRTUSDT","HIGHUSDT","IMXUSDT","LPTUSDT","LQTYUSDT","MAGICUSDT","RDNTUSDT","WOOUSDT"]
 //let pathArr = ["3mv3","5mv3","15mv3","30mv3","1hv3","4hv3"]
 //let itArr = ["3m","5m","15m","30m","1h","4h"]
-let pathArr = ["3m","5m","15m","30m","3mv2","5mv2","15mv2","30mv2","3mv3","5mv3","15mv3","30mv3","1hv3","4hv3"]
-let itArr = ["3m","5m","15m","30m","3m","5m","15m","30m","3m","5m","15m","30m","1h","4h"]
+//let pathArr = ["3m","5m","15m","30m","3mv2","5mv2","15mv2","30mv2","3mv3","5mv3","15mv3","30mv3","1hv3","4hv3"]
+//let itArr = ["3m","5m","15m","30m","3m","5m","15m","30m","3m","5m","15m","30m","1h","4h"]
 
+let pathArr = ["15t"]
+let itArr = ["15m"]
 
 class CoreViewController {
     
@@ -64,7 +66,7 @@ class CoreViewController {
         
         do {
 
-            let path = "/Users/xuanyuan/Downloads/all/\(pathName)/\(sbName)_\(itName).csv"
+            let path = "/Users/xuanyuan/Downloads/\(pathName)/\(sbName)_\(itName).csv"
 
             let csvFileUrl = URL(fileURLWithPath: path)
 
